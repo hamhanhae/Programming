ad1ece6f07150e3a5909a8ddc48a966cbfa64d30
hamhanhae <s2430@e-mirim.hs.kr>
2026-03-26 08:45:13 +0900

    counterHTML, js

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -1,5 +1,8 @@
 const counter = document.querySelector('h1');
 const button = document.querySelector('button');
+const H1result = document.getElementById('H1result');
+const plusButton = document.getElementsByClassName('plusButton')[0];
+// const plusButton = document.querySelector('.plusButton')[0];
 
 let count = 0;
 
