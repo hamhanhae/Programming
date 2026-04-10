8c304feac54ba620d6e318b6224137a65d51a933
hamhanhae <s2430@e-mirim.hs.kr>
2026-04-10 14:37:05 +0900

    TodoListApp HTML className, todolist.css

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,22 +1,26 @@
+import './todoList.css'
+
 function TodoListApp() {
     return (
-        <div>
-            <h1>HyeonDo List</h1>
+        <div className="todo">
+            <h1 className="todo__title">HyeonDo List</h1>
 
-            <form>
-                <input type="text" placeholder="할 일을 입력하세요" />
-                <button type="submit">Add</button>
+            <form className="todo__form">
+                <input type="text" placeholder="할 일을 입력하세요" className="todo__input"/>
+                <button type="submit" className="todo__button todo__button--add">Add</button>
             </form>
             <div>
-                <ul>
-                    <li>
-
-                        <input type="checkbox" id="chk-1"/>
-                        <label htmlFor="chk-1">
+                <ul className="todo__list">
+                    <li className="todo__item todo__item--empty">
+                        <p>할 일 없음.</p>
+                    </li>
+                    <li className="todo__item todo__item--complete">
+                        <input type="checkbox" id="chk-1" className="todo__checkbox"/>
+                        <label htmlFor="chk-1" className="todo__label">
                             <span>지각하기</span>
                         </label>
-                        <button type="button">✏️</button>
-                        <button type="button">❌</button>
+                        <button type="button" className="todo__button todo__button--edit">✏️</button>
+                        <button type="button" className="todo__button todo__button--delete">❌</button>
                     </li>
                     <li>
                         <input type="checkbox" id="chk-2"/>
