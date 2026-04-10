62ac2cff17ffa3dc54830a02e75fed6f06538956
hamhanhae <s2430@e-mirim.hs.kr>
2026-04-10 13:19:43 +0900

    TodoListApp reset.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react' // 리액트에서 가져올게 많으면 중괄호
 import { createRoot } from 'react-dom/client'
 // import './index.css'
+import './reset.css'
 import TodoListApp from './TodoListApp.jsx' // 가져올게 하나밖에 없거나, 가져올게 많더라고 하나라고 지정하면 중괄호를 안 친다.
 
 createRoot(document.getElementById('root')).render(
